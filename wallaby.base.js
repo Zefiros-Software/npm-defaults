236b96f0effb5f6e4a538dbe85cc6974622bdcb3
MickVanDuijn <m.vanduijn@zefiros.eu>
2019-03-29 17:31:22 +0100

    feat(testing): add default configurations for testing

---
+++ wallaby.base.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = function(dir) {
+    return w => ({
+        files: [
+            'tsconfig.json',
+            { pattern: 'jest.config.js', instrument: false },
+            { pattern: 'wallaby.js', instrument: false },
+            'src/**/*.ts',
+            'src/**/*.js',
+            'src/**/*.json',
+        ],
+        filesWithNoCoverageCalculated: ['jest.config.js', 'wallaby.js'],
+
+        tests: ['test/**/*.spec.ts', 'packages/*/test/**/*.spec.ts'],
+
+        env: { type: 'node' },
+
+        testFramework: {
+            type: 'jest',
+            path: path.resolve(dir, 'node_modules/jest-cli'),
+        },
+
+        compilers: {
+            '**/*.ts': w.compilers.typeScript({
+                isolatedModules: true,
+                module: 'commonjs',
+            }),
+        },
+        filesWithNoCoverageCalculated: [],
+
+        setup: function(wallaby) {
+            const jestConfig = require('./jest.config.js')
+            wallaby.testFramework.configure(jestConfig)
+        },
+    })
+}

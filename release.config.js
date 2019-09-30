737e3ca6d9d938f777b93a7998be733a8eff4028
Paul Visscher <p.e.visscher@zefiros.io>
2019-09-30 19:03:59 +0200

    chore: test semantic release

--- release.config.js
+++ release.config.js
@@ -2,7 +2,9 @@ module.exports = {
     branches: [{ name: 'master' }, { name: 'development', channel: 'next', prerelease: 'beta' }],
     plugins: [
         '@semantic-release/commit-analyzer',
+        '@semantic-release/git',
         '@semantic-release/github',
+        '@semantic-release/changelog',
         '@semantic-release/npm',
         '@semantic-release/release-notes-generator',
     ],

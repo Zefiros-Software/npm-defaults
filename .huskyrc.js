e2f5b71c8a5ec5d40fe0d7cd646426d830724317
Paul Visscher <p.e.visscher@zefiros.io>
2019-10-21 19:57:17 +0200

    chore: run tests before push

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,5 +2,7 @@ module.exports = {
     hooks: {
         'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
         'pre-commit': 'yarn fix',
+        'post-commit': 'git update-index --again',
+        'pre-push': 'yarn test',
     },
 }

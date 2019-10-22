3c4f566ba1553315dbd56dc3980b82619f218970
Paul Visscher <p.e.visscher@zefiros.io>
2019-10-23 00:21:33 +0200

    chore: improve tsconfig configuration

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     '**/*.ts': ['yarn fix', 'git add'],
-    '**/*.{js,json}': ['yarn prettier --write', 'git add'],
+    '**/*.{js,json}': ['yarn prettier --write --config prettier.config.js', 'git add'],
 }
